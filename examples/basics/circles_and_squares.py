4b27f77828343cb28d6fcb8754a2b2b728ac185d
Mathijs Schuurmans <mathijs.schuurmans@gmail.com>
2024-10-02 19:02:15 +0200

    [Feat] Draw circles, rectangles, and arbitrary polygons

---
+++ examples/basics/circles_and_squares.py
@@ -0,0 +1,14 @@
+import pykz
+
+
+rect = pykz.rectangle((-1, -1), (1, 1))
+circle = pykz.circle((2, 0), (1), fill="red")
+
+rect2 = pykz.rectangle((1, 1), (2, 3), fill="cyan")
+
+# Dump the generated tikz code to the stdout.
+print(pykz.dumps())
+
+
+# Save the Tikz code to a temporary file, compile it, and open the pdf in the default viewer.
+pykz.preview()
---
+++ pykz/commands/circle.py
@@ -0,0 +1,23 @@
+from .draw import Draw
+from .node import Node
+from .connector import Connector
+
+import numpy as np
+
+
+class Circle(Draw):
+
+    def __init__(self, point: np.ndarray | str | Node, radius: float, **options):
+        self.radius = radius
+        self.center = point
+
+        super().__init__([point, str(radius)], connector=Connector("circle"), **options)
+
+    # Override how points work
+    @property
+    def points(self) -> list[np.ndarray, str]:
+        return [self.center, str(self.radius)]
+
+    @points.setter
+    def points(self, p):
+        pass
