eed76903d65d14d9d4ed91634b686e7a3fc3936d
Mathijs Schuurmans <mathijs.schuurmans@gmail.com>
2025-01-20 11:59:07 +0100

    Add export to png + docstrings

--- pykz/io.py
+++ pykz/io.py
@@ -14,41 +14,138 @@ def __export_to_tempfile(code: str) -> str:
     return f.name
 
 
-def build_latex_code(code: str) -> str:
+def export_pdf_from_code(code: str) -> str:
+    """
+    Compile the given ``tex`` code to a pdf file.
+
+    Use ``pdflatex`` to compile the document to a standalone pdf file.
+
+    Parameters
+    ----------
+    code
+        String representation of the tex to be compiled.
+
+    Returns
+    -------
+    str
+        Path to the generated pdf file.
+    """
     file = __export_to_tempfile(code)
-    return build_latex_file(file)
+    return export_pdf_from_file(file)
 
 
-def build_latex_file(path: str) -> str:
+def export_pdf_from_file(path: str) -> str:
+    """
+    Compile the ``tex`` code at the given path.
+
+
+    Use ``pdflatex`` to compile the document to a standalone pdf file.
+
+    Parameters
+    ----------
+    path
+        The path to the ``tex`` code to be compiled.
+
+    Returns
+    -------
+    str
+        The path to the generated pdf file.
+
+    Raises
+    ------
+    PDFlatexNotFoundError:
+        If pdflatex is not installed.
+    CompilationError:
+        If the given document could not be compiled by pdflatex.
+    """
     import subprocess
     from pykz.exceptions import PDFlatexNotFoundError, CompilationError
     import os
     working_dir = os.path.dirname(path)
 
+    options = dict(capture_output=True, check=True)
+    if working_dir:
+        options["cwd"] = working_dir
+
     try:
-        # Run pdflatex command
-        print("Running pdflatex")
-        subprocess.run(['pdflatex', "-interaction=nonstopmode", "-halt-on-error", path],
-                       capture_output=True,
-                       cwd=working_dir,
-                       check=True)
-        print("Pdflatex done!")
-    except FileNotFoundError:
-        raise PDFlatexNotFoundError(f"pdflatex command not found when trying to compile {path}. Please make sure it is installed and accessible in the system's PATH.")
+        import shutil
+        pdflatex_path = shutil.which('pdflatex')
+        if pdflatex_path is None:
+            raise PDFlatexNotFoundError(f"Could not find executable `pdflatex` to compile {path}. Please make sure it is installed and accessible in the system's path.")
+
+        subprocess.run([pdflatex_path, "-interaction=nonstopmode", "-halt-on-error", path], **options)
+        # print("Pdflatex done!")
     except subprocess.CalledProcessError as e:
         error_message = f"{e.stdout.decode('UTF-8')}\n\nCompilation failed with the error above ☝️ "
         raise CompilationError(error_message)
     import os
 
+    basename = os.path.splitext(path)[0]
     for ext in {".aux", ".log"}:
         try:
-            os.remove(path + ext)
-        except FileNotFoundError():
+            os.remove(basename + ext)
+        except FileNotFoundError:
             ...
-    return f"{path}.pdf"
+    return f"{basename}.pdf"
+
+
+def export_png_from_file(input_file: str, **options) -> str:
+    """
+    Export the given tex file to a png image.
+
+    The tex file is first compiled to pdf using pdflatex. Then the
+    resulting pdf is converted to an image using ``pdf2image``.
+
+    Parameters
+    ----------
+    input_file
+        The path to the ``tex`` code to be compiled.
+
+    Returns
+    -------
+    str
+        The path to the generated png file.
+    """
+    import pdf2image
+    pdf_file = export_pdf_from_file(input_file)
+    path = pdf_file.replace(".pdf", ".png")
+    options["output_file"] = path
+    pdf2image.convert_from_path(pdf_file, **options)
+    return path
+
+
+def export_png_from_code(code: str, path: str, **options):
+    """
+    Export the given tex file to a png image.
+
+    The tex file is first compiled to pdf using pdflatex. Then the
+    resulting pdf is converted to an image using ``pdf2image``.
+
+    Parameters
+    ----------
+    input_file
+        The path to the ``tex`` code to be compiled.
+
+    Returns
+    -------
+    str
+        The path to the generated png file.
+    """
+    import pdf2image
+    pdf_file = export_pdf_from_code(code)
+    options["output_file"] = path
+    pdf2image.convert_from_path(pdf_file, **options)
 
 
 def open_pdf_file(file_path: str):
+    """
+    Open the pdf file at the given path in the system default pdf reader.
+
+    Parameters
+    ----------
+    file_path
+        Path to the pdf file to open.
+    """
     import sys
     import subprocess
     print("Opening pdf file")
@@ -68,7 +165,23 @@ def open_pdf_file(file_path: str):
 
 
 def preview_latex_doc(code: str) -> str:
-    pdf_path = build_latex_code(code)
+    """
+    Preview the given tex code.
+
+    The given code must be a valid tex document that can be compiled.
+    It gets compiled to a temporary file and then opened in the default pdf reader.
+
+    Parameters
+    ----------
+    code
+        The tex to be compiled.
+
+    Returns
+    -------
+    str
+        The path to which the compiled pdf was written. Could be useful for cleanup.
+    """
+    pdf_path = export_pdf_from_code(code)
     print(pdf_path)
 
     try:
